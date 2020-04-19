bb6f66fb7b8893a6b7764f462ea296774fdd1ddb
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-04-18 21:39:09 -0300

    Add donate button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,6 +16,35 @@ class Footer extends Component {
                   Portfolio
                 </a>
               </li>
+              <li>
+                <form
+                  action="https://www.paypal.com/cgi-bin/webscr"
+                  method="post"
+                  target="_top"
+                >
+                  <input type="hidden" name="cmd" value="_s-xclick" />
+                  <input
+                    type="hidden"
+                    name="hosted_button_id"
+                    value="Q9HA3PNRPBQ66"
+                  />
+                  <input
+                    type="image"
+                    src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                    border="0"
+                    name="submit"
+                    title="PayPal - The safer, easier way to pay online!"
+                    alt="Donate with PayPal button"
+                  />
+                  <img
+                    alt=""
+                    border="0"
+                    src="https://www.paypal.com/en_AR/i/scr/pixel.gif"
+                    width="1"
+                    height="1"
+                  />
+                </form>
+              </li>
             </ul>
           </nav>
           <p className="copyright pull-right">
